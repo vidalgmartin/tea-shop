a765994604c05119af84d53e6da6e9582a38f2dc
vidalgmartin <vidalgmartin@hotmail.com>
2024-07-02 16:21:17 -0400

    Server setup

---
+++ server/Models/Item.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
